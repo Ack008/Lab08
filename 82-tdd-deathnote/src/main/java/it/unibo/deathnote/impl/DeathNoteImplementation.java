345279bba3aa71a98e6622daff467e0907ed16a1
ss <francesco.sacripante@studio.unibo.it>
2024-11-12 17:23:03 +0100

    implementing deathNote class methods

--- 82-tdd-deathnote/src/main/java/it/unibo/deathnote/impl/DeathNoteImplementation.java
+++ 82-tdd-deathnote/src/main/java/it/unibo/deathnote/impl/DeathNoteImplementation.java
@@ -1,48 +1,119 @@
 package it.unibo.deathnote.impl;
+import java.util.HashMap;
+import java.util.Map;
+
 import it.unibo.deathnote.api.*;
 
 public class DeathNoteImplementation implements DeathNote{
+    final static private long millisecondsWhithinCanSpecifyCause = 40;
+    final static private long millisecondsWhithinCanSpecifyDetails = 6 * 1000 + 40;
+    private class DeathInfo{
+        public final static String defaultDeathCause = "heart attack";
+        private String deathCause;
+        private String deathDetails;
+        public DeathInfo(){
+            this(defaultDeathCause, "");
+        }
+        public DeathInfo(final String cause, final String deteils){
+            deathCause = cause;
+            deathDetails = deteils;
+        }
+        public String getDeathCause(){
+            return deathCause;
+        }
+
+        public String getDeathDetails(){
+            return deathDetails;
+        }
+
+        public void setCause(final String cause){
+            deathCause = cause;
+        }
+        
+        public void setDetails(final String details){
+            deathDetails = details;
+        }
+        
+    }
+
+    final private Map<String,DeathInfo> writtenName;
+    private String nameOnWhichICanWorkWith;
+    private long timeOfLastWriting;
     public DeathNoteImplementation(){
-
-    }
-    @Override
-    public String getDeathCause(String name) {
-        throw new IllegalAccessError();
+        writtenName = new HashMap<>();
     }
 
     @Override
-    public String getDeathDetails(String name) {
-        throw new IllegalAccessError();
-
+    public String getDeathCause(final String name) {
+        if(!writtenName.containsKey(name)){
+            throw new IllegalArgumentException("Name not written yet");
+        }
+        return writtenName.get(name).getDeathCause();
     }
 
     @Override
-    public String getRule(int ruleNumber) {
-        throw new IllegalAccessError();
-
-    }
-
-    @Override
-    public boolean isNameWritten(String name) {
-        throw new IllegalAccessError();
+    public String getDeathDetails(final String name) {
+        if(!writtenName.containsKey(name)){
+            throw new IllegalArgumentException("Name not written yet");
+        }
+        return writtenName.get(name).getDeathDetails();
 
     }
 
     @Override
-    public boolean writeDeathCause(String cause) {
-        throw new IllegalAccessError();
+    public String getRule(final int ruleNumber) {
+        if(ruleNumber < 1){
+            throw new IllegalArgumentException("rule number < 1");
+        }
+        if(ruleNumber >= DeathNote.RULES.size()){
+            throw new IllegalArgumentException("rule number >= than the number of rules");
+        }
+        return DeathNote.RULES.get(ruleNumber - 1);
+    }
+
+    @Override
+    public boolean isNameWritten(final String name) {
+        return writtenName.containsKey(name);
 
     }
 
     @Override
-    public boolean writeDetails(String details) {
-        throw new IllegalAccessError();
+    public boolean writeDeathCause(final String cause) {
+        if(nameOnWhichICanWorkWith == null || nameOnWhichICanWorkWith.equals("")){
+            throw new IllegalStateException("You haven't yet written a single name on this book");
+        }
+        final long waitedTime = System.currentTimeMillis() - timeOfLastWriting;
+        if(waitedTime <= millisecondsWhithinCanSpecifyCause){
+            writtenName.get(nameOnWhichICanWorkWith).setCause(cause);
+            return true;
+        }
+        return false;
 
     }
 
     @Override
-    public void writeName(String name) {
-        throw new IllegalAccessError();
+    public boolean writeDetails(final String details) {
+        if(nameOnWhichICanWorkWith == null || nameOnWhichICanWorkWith.equals("")){
+            throw new IllegalStateException("you haven't written a name yet");
+        }
+
+        final long waitedTime = System.currentTimeMillis() - timeOfLastWriting;
+        if(waitedTime <= millisecondsWhithinCanSpecifyCause){
+            writtenName.get(nameOnWhichICanWorkWith).setDetails(details);
+            return true;
+        }
+        return false;
+
+    }
+
+    @Override
+    public void writeName(final String name) {
+        if(this.isNameWritten(name)){
+            throw new IllegalArgumentException("Name already written");
+        }
+        writtenName.put(name, new DeathInfo());
+        nameOnWhichICanWorkWith = name;
+        timeOfLastWriting = System.currentTimeMillis();
     }
     
 }
