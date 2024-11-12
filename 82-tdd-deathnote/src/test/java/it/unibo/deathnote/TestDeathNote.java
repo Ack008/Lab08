bd9612ffb946c1a88115ed9773b3eedf90774312
ss <francesco.sacripante@studio.unibo.it>
2024-11-12 16:48:16 +0100

    Tests of es 82 implemented

--- 82-tdd-deathnote/src/test/java/it/unibo/deathnote/TestDeathNote.java
+++ 82-tdd-deathnote/src/test/java/it/unibo/deathnote/TestDeathNote.java
@@ -12,7 +12,22 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
 
 class TestDeathNote {
-    DeathNote deathNote;
+    private DeathNote deathNote;
+    final String name2 = "Mario Draghi";
+    final String name1 = "Antonio Conte";
+
+
+    /*A method that stops for a fixed amount of milliseconds 
+     * @param milliseconds millisecondas where the programs sleeps 
+     * 
+    */
+    private void sleep(long milliseconds){
+        try{
+            Thread.sleep(100);
+        }catch(InterruptedException e){
+        }
+    }
+
     @BeforeEach
     void setup(){
         deathNote = new DeathNoteImplementation();
@@ -38,4 +53,57 @@ class TestDeathNote {
             assertNotEquals(msg, "");
         }
     }
+    
+    @Test
+    void test2(){
+        for (final var rule : DeathNote.RULES) {
+            assertNotNull(rule);
+            assertNotEquals(rule, "");
+        }
+    }
+
+    @Test
+    void test3(){
+        assertFalse(deathNote.isNameWritten(name1));
+        deathNote.writeName(name1);
+        assertTrue(deathNote.isNameWritten(name1));
+        assertFalse(deathNote.isNameWritten(name2));
+        assertFalse(deathNote.isNameWritten(""));
+    }
+
+    @Test
+    void test4(){
+        final String causeOfDeath1 = "karting accident";
+        final String defaultCauseOfDeath = "heart attack";
+        try{
+            deathNote.writeDeathCause("Cancer");
+        }catch(IllegalStateException e){
+        }
+
+        deathNote.writeName(name1);
+        assertEquals(deathNote.getDeathCause(name1).toLowerCase(), defaultCauseOfDeath.toLowerCase());
+        deathNote.writeName(name2);
+        assertTrue(deathNote.writeDeathCause(causeOfDeath1));
+        assertEquals(deathNote.getDeathCause(name2).toLowerCase(), causeOfDeath1.toLowerCase());
+        sleep(100);
+        assertFalse(deathNote.writeDeathCause("Hit by a car"));
+    }
+
+    @Test
+    void test5(){
+        final String detail1 = "ran for too long";
+        final String detail2 = "Neightbours hears everithing";
+        try {
+            deathNote.writeDetails(detail2);
+        } catch (IllegalStateException e) {
+        }
+
+        deathNote.writeName(name1);
+        assertTrue(deathNote.writeDetails(detail1));
+        assertEquals(deathNote.getDeathDetails(name1), detail1);
+        deathNote.writeName(name2);
+        sleep(6100);
+        assertFalse(deathNote.writeDetails(detail2));
+    }
 }
+
